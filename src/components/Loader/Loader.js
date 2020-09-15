cdcc69117327cbb914f8caf9533d3cdb26c52d40
Alexander Loktionov <loktionov129@gmail.com>
2020-09-16 02:54:49 +0300

    add Loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loader.css';
+
+export default function Loader() {
+  return (
+    <img src="/assets/images/loader.svg" className="Loader" alt="logo" />
+  );
+}
