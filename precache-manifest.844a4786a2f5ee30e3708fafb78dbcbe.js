c247c85b5fdfffe04ec8cdfbf1f88889f79c0597
loktionov129 <loktionov129@users.noreply.github.com>
2020-09-18 23:25:49 +0000

    deploy: 6e9e19953c6a14844c9e2dbfcca1965a0878174f

---
+++ precache-manifest.844a4786a2f5ee30e3708fafb78dbcbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3de9ebc8332b6f1d39cca7af76a26bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c743b5022397783cc8",
+    "url": "/static/css/main.49856a81.chunk.css"
+  },
+  {
+    "revision": "f3c9e78bb9fda5333232",
+    "url": "/static/js/2.8ab19968.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ab19968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c743b5022397783cc8",
+    "url": "/static/js/main.1732e1c5.chunk.js"
+  },
+  {
+    "revision": "2544035910ea71fff849",
+    "url": "/static/js/runtime-main.5c5bdc4f.js"
+  }
+]);
