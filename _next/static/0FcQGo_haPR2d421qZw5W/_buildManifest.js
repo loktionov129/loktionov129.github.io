a6098aea7f715db6a43b47ac7b5ed828891ada6f
loktionov129 <loktionov129@users.noreply.github.com>
2021-01-11 16:44:38 +0000

    deploy: 9d5c5ae7112f68c2cb207503c1d1e69490212cfe

---
+++ _next/static/0FcQGo_haPR2d421qZw5W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7ea2a34f6c32ad892987.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-2eaaf871987655b6e19b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
