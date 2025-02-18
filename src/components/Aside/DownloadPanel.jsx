563fc33afc1308c3c853df98f584cbae73f06b27
Aleksandr Loktionov <loktionov129@gmail.com>
2025-02-19 01:27:47 +0700

    Update DownloadPanel.jsx

--- src/components/Aside/DownloadPanel.jsx
+++ src/components/Aside/DownloadPanel.jsx
@@ -11,8 +11,7 @@ function DownloadPanel({ locale }) {
             tabIndex="0"
             role="button"
             className="Button secondary"
-            href="/assets/cv.aleksandr_loktionov.net-developer.pdf"
-            download="cv.aleksandr_loktionov.net-developer.pdf"
+            href="https://abakan.hh.ru/resume/de798b26ff08ca01e40039ed1f4c6f53493774?print=true"
           >
             {locale.downloadCv}
           </a>
