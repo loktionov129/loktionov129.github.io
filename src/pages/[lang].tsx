b48686c6273c85abbed5e7dac7998873f5018b64
Aleksandr Loktionov <loktionov129@gmail.com>
2026-05-07 20:38:32 +0700

    fix

--- src/pages/[lang].tsx
+++ src/pages/[lang].tsx
@@ -1,31 +1,37 @@
 import type { GetStaticProps, GetStaticPaths } from 'next';
 import { createStaticProps } from '@/utils/locale';
-import type { LangContextType } from '@/context';
+import { LangContext, type LangContextType } from '@/context';
 import { Profile } from '@/components/Profile';
 import { KeyWords } from '@/components/KeyWords';
 import { WorkExperience } from '@/components/WorkExperience';
 import { ProjectList } from '@/components/ProjectList';
 import { AboutMe } from '@/components/AboutMe';
 import { AboutApp } from '@/components/AboutApp';
+import { Aside } from '@/components/Aside';
 
-export default function LangPage() {
+export default function LangPage({ lang, t }: LangContextType) {
   return (
-    <>
+    <LangContext.Provider value={{ lang, t }}>
       <title>CV — Aleksandr Loktionov</title>
-      <Profile />
-      <div className='no-print'>
-        <KeyWords />
-        <WorkExperience />
-        <ProjectList />
-        <AboutMe />
-        <AboutApp />
+      <div className='Page'>
+        <article className='Page__Article'>
+          <Profile />
+          <div className='no-print'>
+            <KeyWords />
+            <WorkExperience />
+            <ProjectList />
+            <AboutMe />
+            <AboutApp />
+          </div>
+          <div className='print'>
+            <AboutMe />
+            <ProjectList />
+            <WorkExperience />
+          </div>
+        </article>
       </div>
-      <div className='print'>
-        <AboutMe />
-        <ProjectList />
-        <WorkExperience />
-      </div>
-    </>
+      <Aside />
+    </LangContext.Provider>
   );
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,5 @@
-import { Aside } from '@/components/Aside';
-import { LangContext, type LangContextType } from '@/context';
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps<LangContextType>) {
-  const { t, lang } = pageProps;
-
-  return (
-    t && (
-      <LangContext.Provider value={{ lang, t }}>
-        <div className='Page'>
-          <article className='Page__Article'>
-            <Component {...pageProps} />
-          </article>
-        </div>
-        <Aside />
-      </LangContext.Provider>
-    )
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
