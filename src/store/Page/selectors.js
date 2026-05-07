caf13888404c9b9d7f3bf2c53674b1d2871cd4d5
Aleksandr Loktionov <loktionov129@gmail.com>
2026-05-07 11:29:05 +0700

    rm -rf /

--- src/store/Page/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { getLang } from '../Aside/selectors';
-
-const getData = (state) => state.page.data;
-export const getProfile = createSelector(
-  getLang,
-  getData,
-  (lang, data) => data[lang],
-);
